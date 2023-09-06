6561d7d1874bcde177935857db8fa72d60218fa2
Marcin Sroczyński <113846168+Marcin-Sroczynski@users.noreply.github.com>
2023-09-06 14:19:31 +0200

    Create DogAge_calculator.js

---
+++ DogAge_calculator.js
@@ -0,0 +1,15 @@
+var myAge = 40;
+//my current age 
+
+let earlyYears = 2;
+earlyYears *= 10.5;
+
+let laterYears = myAge -2;
+laterYears *= 4;
+//dog years
+//console.log(earlyYears,laterYears);
+var myAgeInDogYears = earlyYears+laterYears;
+
+var myName = 'Marcin'.toLowerCase();
+//console.log(myName);
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
